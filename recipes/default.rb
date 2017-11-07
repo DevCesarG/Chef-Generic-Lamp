2b58b93280738bdc3e1a3743fc1855f2a6d619ce
Cesar Garcia <cesbeast@gmail.com>
2017-11-06 17:34:24 -0800

    Bring up a lamp stack using chef kitchen

---
+++ recipes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook:: lamp
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
+apt_update 'Update the apt cache daily' do
+  frequency 86_400
+  action :periodic
+end
+
+include_recipe 'lamp::web'
+include_recipe 'lamp::database'
